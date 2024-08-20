b33ab7339a7f75975eba63380d79dd1456667fd4
DiMNDev <Dimndev@gmail.com>
2024-08-20 14:55:03 -0600

    moved NavLinkData to layout / updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,32 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
+import { NavLinks, linkNames, linkRefs } from "./types";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "useContext()",
-  description: "When to use useContext()",
+  title: "Prop-Drilling & LocalStorage",
+  description: "There is a better way",
 };
-
+const NavLinkData: NavLinks = [
+  {
+    linkName: linkNames.home,
+    linkHref: linkRefs.home,
+  },
+  {
+    linkName: linkNames.products,
+    linkHref: linkRefs.products,
+  },
+  {
+    linkName: linkNames.cart,
+    linkHref: linkRefs.cart,
+  },
+  {
+    linkName: linkNames.login,
+    linkHref: linkRefs.login,
+  },
+];
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -18,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        <Navbar NavLinkData={NavLinkData} />
         {children}
       </body>
     </html>
